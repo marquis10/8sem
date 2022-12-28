9e8b8a12e2c303d1e9092eaa0ad6299c639a2080
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-28 22:07:36 +0500

    new

--- 57z/Program.cs
+++ 57z/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1,11);
+            Console.Write($"{matrix[i,j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int count = 0;
+            if (matrix[i, j] == matrix[i, j+1] || matrix[i, j] == matrix[i+1, j])
+            {
+                count += 1;
+            }
+            Console.WriteLine($"{matrix[i, j]} встречается {count} раз");
+        }
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0],size[1]];
+Console.WriteLine("Начальная матрица");
+InputMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
