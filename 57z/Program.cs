9b3943cf0af625044642058e9c24afcd50224ef2
Alexandr Kuzmin <kuzmin_89@mail.ru>
2022-12-29 22:14:08 +0500

    57zs

--- 57z/Program.cs
+++ 57z/Program.cs
@@ -1,37 +1,47 @@
-﻿void InputMatrix(int[,] matrix)
+﻿void InputMatrix(int[,] matrix, int[] temp)
 {
+    int count = 0;
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             matrix[i,j] = new Random().Next(1,11);
             Console.Write($"{matrix[i,j]} \t");
+            temp[count] = matrix[i, j];
+            count++;
         }
     Console.WriteLine();
     }
 }
 
-void ReleaseMatrix(int[,] matrix)
+void ReleaseMatrix(int[] temp)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < temp.Length; i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        int count = 0;
+        int c = 0;
+        for (int j = 0; j < temp.Length; j++)
         {
-            int count = 0;
-            if (matrix[i, j] == matrix[i, j+1] || matrix[i, j] == matrix[i+1, j])
-            {
-                count += 1;
-            }
-            Console.WriteLine($"{matrix[i, j]} встречается {count} раз");
+            if (temp[i] == temp[j])
+                count++;
         }
+        for (int a = 0; a < i; a++)
+        {
+            if (temp[i] == temp[a])
+                c++;
+        }
+        if (c == 0)
+            Console.WriteLine($"число {temp[i]} встречается {count} раз");
     }
 }
 
 Console.Clear();
 Console.WriteLine("Введите размер матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
-int[,] matrix = new int[size[0],size[1]];
+int[,] matrix = new int[size[0], size[1]];
+int[] temp = new int[size[0] * size[1]];
 Console.WriteLine("Начальная матрица");
-InputMatrix(matrix);
+InputMatrix(matrix, temp);
 Console.WriteLine();
-ReleaseMatrix(matrix);
+Console.WriteLine($"Временный массив: [{string.Join(", ", temp)}]");
+ReleaseMatrix(temp);
